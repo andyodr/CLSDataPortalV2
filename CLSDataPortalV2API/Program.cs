667315a284d37410fc838ff9dc0fe56aefb9fbe7
Andy Duran <andyodr@gmail.com>
2023-01-27 15:36:35 -0500

    UI and API Working Skeleton

--- CLSDataPortalV2API/Program.cs
+++ CLSDataPortalV2API/Program.cs
@@ -1,12 +1,32 @@
+using CLSDataPortalV2API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options => 
+{
+    //Use SQL Server for production 
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 app.MapControllers();
 
