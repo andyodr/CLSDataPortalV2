f2a2ff7475a5884c94b98d0f130bca12a1a04624
Tim Fisher <tfisher@itgtec.com>
2023-02-05 22:55:58 -0500

    Data-imports controllers

---
+++ CLS.WebApi/Controllers/DataImports/DataImportsFilterController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace CLS.WebApi.Controllers.DataImports;
+
+[Authorize]
+[Route("api/[controller]")]
+public class FilterController : ControllerBase
+{
+	private IMeasureTypeRepository _measureTypeRepository;
+	private ICalendarRepository _calendarRepository;
+	private IErrorLogRepository _errorLogRepository;
+	private UserObject _user = new UserObject();
+
+	public FilterController(IMeasureTypeRepository measureTypeRepository, ICalendarRepository calendarRepository, IErrorLogRepository errorLogRepository) {
+		_measureTypeRepository = measureTypeRepository;
+		_calendarRepository = calendarRepository;
+		_errorLogRepository = errorLogRepository;
+	}
+
+	// GET: api/values
+	[HttpGet]
+	public IEnumerable<DataImportFilterGetAllObject> Get(int intervalId, int year) {
+		int errorId = 0;
+		string errorMessage = string.Empty;
+		try {
+			_user = Helper.UserAuthorization(User);
+			if (_user == null)
+				throw new Exception();
+			if (!Helper.IsUserPageAuthorized(Helper.pages.dataImports, _user.userRoleId))
+				throw new Exception(Resource.PAGE_AUTHORIZATION_ERR);
+
+			var calendarRecords = _calendarRepository.All().Where(c => c.Interval.Id == intervalId && c.Year == year);
+			if (intervalId == (int)Helper.intervals.weekly) {
+				return calendarRecords.Select(c => new DataImportFilterGetAllObject {
+					id = c.Id,
+					number = c.WeekNumber,
+					startDate = c.StartDate,
+					endDate = c.EndDate,
+					month = null
+				});
+			}
+			else if (intervalId == (int)Helper.intervals.quarterly) {
+				return calendarRecords.Select(c => new DataImportFilterGetAllObject {
+					id = c.Id,
+					number = c.Quarter,
+					startDate = c.StartDate,
+					endDate = c.EndDate,
+					month = null
+				});
+			}
+			else if (intervalId == (int)Helper.intervals.monthly) {
+				return calendarRecords.Select(c => new DataImportFilterGetAllObject {
+					id = c.Id,
+					number = c.Month,
+					startDate = null,
+					endDate = null,
+					month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName((int)c.Month)
+				});
+			}
+			else throw new Exception(Resource.DI_FILTER_INVALID_INTERVAL);
+		}
+		catch (Exception e) {
+			errorId = _errorLogRepository.logError(e.Message, e.InnerException, e.StackTrace);
+			errorMessage = e.Message;
+			DataImportFilterGetAllObject returnObject = new DataImportFilterGetAllObject();
+			returnObject.error.id = errorId;
+			returnObject.error.message = errorMessage;
+			return (IEnumerable<DataImportFilterGetAllObject>)returnObject;
+		}
+	}
+
+}
---
+++ CLS.WebApi/Controllers/DataImports/DataImportsIndexController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CLS.WebApi.Controllers.DataImports;
+
+[Authorize]
+[Route("api/dataimports/[controller]")]
+public class IndexController : ControllerBase
+{
+	private IIntervalRepository _intervalRepository;
+	private ICalendarRepository _calendarRepository;
+	private IAuditTrailRepository _auditTrailRepository;
+	private IMeasureDataRepository _measureDataRepository;
+	private IMeasureRepository _measureRepository;
+	private ISettingRepository _settingRepository;
+	private UserObject _user = new UserObject();
+
+	public IndexController(IIntervalRepository intervalRepository
+						 , ICalendarRepository calendarRepository
+						 , IAuditTrailRepository auditTrailRepository
+						 , IMeasureDataRepository measureDataRepository
+						 , IMeasureRepository measureRepository
+						 , ISettingRepository settingRepository) {
+		_intervalRepository = intervalRepository;
+		_calendarRepository = calendarRepository;
+		_auditTrailRepository = auditTrailRepository;
+		_measureDataRepository = measureDataRepository;
+		_measureRepository = measureRepository;
+		_settingRepository = settingRepository;
+	}
+
+	// GET: api/values
+	[HttpGet]
+	public string Get() {
+
+		try {
+			_user = Helper.UserAuthorization(User);
+			if (_user == null)
+				throw new Exception();
+			if (!Helper.IsUserPageAuthorized(Helper.pages.dataImports, _user.userRoleId))
+				throw new Exception(Resource.PAGE_AUTHORIZATION_ERR);
+
+			DataImportsMainObject returnObject = new DataImportsMainObject {
+				years = new List<ViewModel.FilterObjects.YearsObject>(),
+				//calculationTime = new CalculationTimeObject(),
+				calculationTime = "00:01:00",
+				dataImport = new List<DataImportObject>(),
+				intervals = new List<intervalsObject>()
+			};
+
+			returnObject.intervalId = Helper.defaultIntervalId;
+			returnObject.calendarId = Helper.FindPreviousCalendarId(_calendarRepository, Helper.defaultIntervalId);
+
+			//returnObject.calculationTime.current = DateTime.Now;
+			string sCalculationTime = _settingRepository.All().FirstOrDefault().CalculateSchedule;
+			returnObject.calculationTime = Helper.CalculateScheduleStr(sCalculationTime, "HH", ":") + " Hours, " +
+										   Helper.CalculateScheduleStr(sCalculationTime, "MM", ":") + " Minutes, " +
+										   Helper.CalculateScheduleStr(sCalculationTime, "SS", ":") + " Seconds";
+
+			//years
+			var years = _calendarRepository.All()
+						.Where(c => c.Interval.Id == (int)Helper.intervals.yearly)
+						.OrderByDescending(y => y.Year);
+
+			foreach (var year in years) {
+				returnObject.years.Add(new ViewModel.FilterObjects.YearsObject { year = year.Year, id = year.Id });
+			}
+
+			// Find Current Year from previuos default interval
+			var calendarId = Helper.FindPreviousCalendarId(_calendarRepository, Helper.defaultIntervalId);
+			returnObject.currentYear = _calendarRepository.All().Where(c => c.Id == calendarId).FirstOrDefault().Year;
+
+			//intervals
+			var intervals = _intervalRepository.All();
+			foreach (var interval in intervals) {
+				returnObject.intervals.Add(new intervalsObject {
+					id = interval.Id,
+					name = interval.Name
+				});
+			}
+
+			//dataImport
+			DataImportObject measureData = Helper.DataImportHeading(Helper.dataImports.measureData);
+			returnObject.dataImport.Add(measureData);
+
+			if (_user.userRoleId == (int)Helper.userRoles.systemAdministrator) {
+				DataImportObject targetData = Helper.DataImportHeading(Helper.dataImports.target);
+				returnObject.dataImport.Add(targetData);
+
+				// This is for kris only
+				if (Startup.ConfigurationJson.usesCustomer) {
+					DataImportObject customerRegionData = Helper.DataImportHeading(Helper.dataImports.customer);
+					returnObject.dataImport.Add(customerRegionData);
+				}
+
+			}
+
+			return Newtonsoft.Json.JsonConvert.SerializeObject(returnObject);
+		}
+		catch (Exception e) {
+			return Helper.errorProcessing(e, _auditTrailRepository, HttpContext, _user);
+		}
+	}
+
+	// GET api/values/5
+	[HttpGet("{id}")]
+	public string Get(int id) {
+		return "value";
+	}
+
+	// POST api/values
+	[HttpPost]
+	public void Post([FromBody] string value) {
+	}
+
+	// PUT api/values/5
+	[HttpPut("{id}")]
+	public void Put(int id, [FromBody] string value) {
+	}
+
+	// DELETE api/values/5
+	[HttpDelete("{id}")]
+	public void Delete(int id) {
+	}
+
+}
