6cb1d4f9d7dc27e834f729e7ebc199eb09b4457f
Tim Fisher <tfisher@itgtec.com>
2023-02-28 15:10:46 -0500

    organize imports & declarations

--- CLS/src/app/app.module.ts
+++ CLS/src/app/app.module.ts
@@ -1,84 +1,82 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppDialog } from './app-dialog.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { MemberListComponent } from './members/member-list/member-list.component';
-import { MemberDetailComponent } from './members/member-detail/member-detail.component';
-import { ListsComponent } from './lists/lists.component';
-import { MessagesComponent } from './messages/messages.component';
-import { SharedModule } from './_modules/shared.module';
-import { TestErrorComponent } from './errors/test-error/test-error.component';
-import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { DataImportsComponent } from './dataimports/dataimports.component';
+import { MultipleSheetsDialog } from './dataimports/multiplesheets-dialog.component';
+import { ErrorsComponent } from './errors/errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
-import { SideComponent } from './side/side.component';
-import { DataImportsComponent } from './dataimports/dataimports.component';
-import { TargetsComponent } from './targets/targets.component';
-import { MeasuresComponent } from './measures/measures.component';
-import { MeasureDefinitionComponent } from './measuredefinition/measuredefinition.component';
-import { HierarchyComponent } from './hierarchy/hierarchy.component';
-import { ErrorsComponent } from './errors/errors.component';
-import { TableComponent } from './table/table.component';
+import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { FilterPipe } from './filter.pipe';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MatDialogModule } from '@angular/material/dialog'
-import { MatRadioModule } from '@angular/material/radio'
-import { MatSelectModule } from '@angular/material/select'
-import { MatProgressBarModule } from '@angular/material/progress-bar'
-import { MultipleSheetsDialog } from './dataimports/multiplesheets-dialog.component'
-import { AppDialog } from './app-dialog.component'
+import { HierarchyComponent } from './hierarchy/hierarchy.component';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MeasureDefinitionComponent } from './measuredefinition/measuredefinition.component';
+import { MeasuresComponent } from './measures/measures.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { NavComponent } from './nav/nav.component';
+import { RegisterComponent } from './register/register.component';
+import { SideComponent } from './side/side.component';
+import { TableComponent } from './table/table.component';
+import { TargetsComponent } from './targets/targets.component';
 import { UserListComponent } from './users/user-list/user-list.component';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavComponent,
-        HomeComponent,
-        RegisterComponent,
-        MemberListComponent,
-        MemberDetailComponent,
-        ListsComponent,
-        MessagesComponent,
-        MeasureDataComponent,
-        TargetsComponent,
-        MeasuresComponent,
-        MeasureDefinitionComponent,
-        HierarchyComponent,
-        DataImportsComponent,
-        TestErrorComponent,
-        NotFoundComponent,
-        ServerErrorComponent,
-        SideComponent,
-        LoginComponent,
-        ErrorsComponent,
-        TableComponent,
-        FilterPipe,
-        MultipleSheetsDialog,
-        AppDialog,
-        UserListComponent
-    ],
-    imports: [
-        BrowserAnimationsModule,
-        BrowserModule,
-        AppRoutingModule,
-        HttpClientModule,
-        FormsModule,
-        SharedModule,
-        NgbModule,
-        MatDialogModule,
-        MatRadioModule,
-        MatSelectModule,
-        MatProgressBarModule
-    ],
-    providers: [
-        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-    ],
-    bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		AppDialog,
+		DataImportsComponent,
+		ErrorsComponent,
+		FilterPipe,
+		HierarchyComponent,
+		HomeComponent,
+		ListsComponent,
+		MeasureDefinitionComponent,
+		MeasuresComponent,
+		MemberDetailComponent,
+		MemberListComponent,
+		MessagesComponent,
+		MultipleSheetsDialog,
+		NavComponent,
+		NotFoundComponent,
+		RegisterComponent,
+		ServerErrorComponent,
+		SideComponent,
+		TableComponent,
+		TargetsComponent,
+		TestErrorComponent,
+		UserListComponent
+	],
+	imports: [
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		BrowserModule,
+		FormsModule,
+		HttpClientModule,
+		MatDialogModule,
+		MatProgressBarModule,
+		MatRadioModule,
+		MatSelectModule,
+		NgbModule,
+		SharedModule
+	],
+	providers: [
+		{ provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
