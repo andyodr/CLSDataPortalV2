9fcf7b91e1e23e0f0535fc4c53b4013f5d3333ed
Tim Fisher <tfisher@itgtec.com>
2023-03-03 01:19:23 -0500

    add icons to DataImportsComponent

--- CLS/src/app/app.module.ts
+++ CLS/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatButtonModule } from "@angular/material/button"
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from '@angular/material/icon'
+import { MatProgressBarModule } from '@angular/material/progress-bar'
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
@@ -28,39 +31,42 @@ import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { SharedModule } from './_modules/shared.module';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		AppDialog,
-		DataImportsComponent,
-		ErrorsComponent,
-		FilterPipe,
-		HomeComponent,
-		MeasureDefinitionComponent,
-		MultipleSheetsDialog,
-		NotFoundComponent,
-		ServerErrorComponent,
-		TableComponent,
+    declarations: [
+        AppComponent,
+        AppDialog,
+        DataImportsComponent,
+        ErrorsComponent,
+        FilterPipe,
+        HomeComponent,
+        MeasureDefinitionComponent,
+        MultipleSheetsDialog,
+        NotFoundComponent,
+        ServerErrorComponent,
+        TableComponent,
         TestErrorComponent,
         UploadDirective,
-		UserListComponent
-	],
-	imports: [
-		AppRoutingModule,
-		BrowserAnimationsModule,
-		BrowserModule,
-		FormsModule,
-		HttpClientModule,
-		MatDialogModule,
-		MatProgressBarModule,
-		MatRadioModule,
+        UserListComponent
+    ],
+    imports: [
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        MatButtonModule,
+        MatDialogModule,
+        MatFormFieldModule,
+        MatIconModule,
+        MatProgressBarModule,
+        MatRadioModule,
         MatSelectModule,
         MatSnackBarModule,
-		NgbModule,
-		SharedModule
-	],
-	providers: [
-		{ provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
-	],
-	bootstrap: [AppComponent]
+        NgbModule,
+        SharedModule
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
