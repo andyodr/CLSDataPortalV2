96cdd83d6a314a1ef03e9c3cbaf8666f945003a5
Tim Fisher <tfisher@itgtec.com>
2023-04-12 14:19:09 -0400

    move/rename CLS project to Deliver

--- CLS.WebApi/Data/Models/ApplicationUser.cs
+++
@@ -1,7 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace CLS.WebApi.Data.Models;
-
-public class ApplicationUser : IdentityUser
-{
-}
--- Sandbox/Models/UserRole.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CLS.WebApi.Data.Models;
-
-public class UserRole
-{
-	public int Id { set; get; }
-	public string Name { set; get; } = null!;
-}
