af782084decca93e92302eac60fe482bfccd0261
Tim Fisher <tfisher@itgtec.com>
2024-03-24 18:12:03 -0400

    more lazy-loaded components

--- Deliver/src/app/app-routing.module.ts
+++ Deliver/src/app/app-routing.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MeasureDataComponent } from './measure-data/measure-data.component';
-import { MeasureDefinitionComponent } from './measuredefinition/measuredefinition.component';
-import { MeasureDefinitionEditComponent } from "./measuredefinition/measuredefinition-edit.component"
 import { DataImportsComponent } from './dataimports/dataimports.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { AuthGuard } from './_guards/auth.guard';
-import { TargetsComponent } from './targets/targets.component';
 
 const title = "DELIVER - "
 
@@ -26,30 +23,38 @@ export const routes: Routes = [
         ]
     },
     {
-        path: "settings",
-        title: `${ title }Settings`,
+        path: "settings", title: `${ title }Settings`,
         loadComponent: () => import("./calendar/settings.component").then(m => m.CalendarSettingsComponent)
     },
     {
-        path: "hierarchy",
-        title: `${ title }Region Hierarchy`,
+        path: "hierarchy", title: `${ title }Region Hierarchy`,
         loadComponent: () => import("./hierarchy/hierarchy.component").then(m => m.RegionHierarchyComponent) },
     {
         path: '',
         runGuardsAndResolvers: 'always',
         canActivate: [AuthGuard], children: []
     },
-    { path: 'targets', title: `${ title }Targets`, component: TargetsComponent },
     {
-        path: 'measures',
-        title: `${ title }Measures`,
+        path: 'targets', title: `${ title }Targets`,
+        loadComponent: () => import("./targets/targets.component").then(m => m.TargetsComponent) },
+    {
+        path: 'measures', title: `${ title }Measures`,
         loadComponent: () => import("./measures/measures.component").then(m => m.MeasuresComponent)
     },
     {
         path: "measuredefinition", children: [
-            { path: "", title: `${ title }Measure Definitions`, component: MeasureDefinitionComponent },
-            { path: "add", title: `${ title }Add Measure Definition`, component: MeasureDefinitionEditComponent },
-            { path: ":id", title: `${ title }Edit Measure Definition`, component: MeasureDefinitionEditComponent },
+            {
+                path: "", title: `${ title }Measure Definitions`,
+                loadComponent: () => import("./measuredefinition/measuredefinition.component").then(m => m.MeasureDefinitionComponent)
+            },
+            {
+                path: "add", title: `${ title }Add Measure Definition`,
+                loadComponent: () => import("./measuredefinition/measuredefinition-edit.component").then(m => m.MeasureDefinitionEditComponent)
+            },
+            {
+                path: ":id", title: `${ title }Edit Measure Definition`,
+                loadComponent: () => import("./measuredefinition/measuredefinition-edit.component").then(m => m.MeasureDefinitionEditComponent)
+            },
         ]
     },
     { path: "dataimports", title: `${ title }Data Imports`, component: DataImportsComponent },
